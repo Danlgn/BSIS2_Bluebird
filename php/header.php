91502ee40699c34364114740e031ea52faf82723
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-05-31 23:17:37 +0800

    exit test

--- php/header.php
+++ php/header.php
@@ -6,8 +6,7 @@
         $user_details = mysqli_query($con, "SELECT * FROM users where username='{$userLoggedIn}'");
         $user = mysqli_fetch_array($user_details);
     } else {
-        header("Location: login.php");
-        exit();
+        exit(header("Location: login.php"));
     }
     
 ?>
