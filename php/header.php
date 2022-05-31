2cd35da3580b84e900d8624955c4c4da5168838d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-05-31 23:22:57 +0800

    debug header 2

--- php/header.php
+++ php/header.php
@@ -1,6 +1,6 @@
 <?php
+    session_start();
     require('db.php');
-
     if(isset($_SESSION["username"])) {
         $userLoggedIn = $_SESSION["username"];
         $user_details = mysqli_query($con, "SELECT * FROM users where username='{$userLoggedIn}'");
