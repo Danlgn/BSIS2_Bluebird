046e03d464d45880572e07799763020c8b2eff34
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-05-31 23:25:21 +0800

    debug header 3

--- php/header.php
+++ php/header.php
@@ -5,9 +5,9 @@
         $userLoggedIn = $_SESSION["username"];
         $user_details = mysqli_query($con, "SELECT * FROM users where username='{$userLoggedIn}'");
         $user = mysqli_fetch_array($user_details);
-    } else {
         echo "<div>hello?".$_SESSION['username']."</div>";
-        //exit(header("Location: login.php"));
+    } else {
+        exit(header("Location: login.php"));
     }
     
 ?>
